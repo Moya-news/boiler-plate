3443bf0bd798947a049bfb074a7df71b75d93d1f
ParkSungHyun <psh1030.dev@gmail.com>
2022-02-15 23:15:18 +0900

    Comment: add comment
    add comments related to component creation

--- src/components/example/index.js
+++ src/components/example/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ExampleDiv } from "./style";
+import { ExampleWrapper } from "./style";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCount } from "../../reducers/counter/actionType";
 const Example = ({ exampleMessage }) => {
@@ -10,15 +10,18 @@ const Example = ({ exampleMessage }) => {
     dispatch(increaseCount(1));
   };
   return (
-    <ExampleDiv>
-      <div>{count}</div>
+    <ExampleWrapper>
+      <div className="count_holder">{count}</div>
       <div>
         <button onClick={onClickButton}>누르면 카운트가 증가해요</button>
       </div>
       <div>{exampleMessage}</div>
-    </ExampleDiv>
+    </ExampleWrapper>
   );
 };
+
+// Typescript를 적용하지 않은 대신 propTypes를 사용하여 안정성을 더했습니다.
+// prop:PropsTypes.type 의 형태로 사용해주시면 됩니다.
 
 Example.propTypes = {
   exampleMessage: PropTypes.string,
--- src/components/example/style.js
+++ src/components/example/style.js
@@ -1,3 +1,15 @@
 import styled from "@emotion/styled";
 
-export const ExampleDiv = styled.div``;
+// 큰 단위를 styled.div로 만들고 내부의 요소들은 css 선택자로 관리해주시면 좋을 것 같습니다.
+// 필요에 따라 여러개의 css-in-js 요소를 선언하여 사용하셔도 좋을 것 같습니다.
+
+export const ExampleWrapper = styled.div`
+  & > div {
+    height: 100px;
+  }
+  & > .count_holder {
+    border: 1px solid black;
+    text-align: center;
+    vertical-align: middle;
+  }
+`;
