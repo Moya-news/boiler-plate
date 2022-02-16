8216d4b7945ab339b06a7fc3f52b73566bd8126c
ParkSungHyun <psh1030.dev@gmail.com>
2022-02-16 21:05:26 +0900

    Comment: add description

--- src/sagas/counter.js
+++ src/sagas/counter.js
@@ -6,6 +6,8 @@ import {
 } from "../reducers/counter/actionType";
 
 // 제너레이터 함수를 사용하여 작성해주시면 됩니다.
+// delay를 통해 비동기 호출을 임의로 재현해보았습니다.
+// 실제 프로덕트를 만들 때는 이 부분에 api 호출로 데이터를 받아오도록 하면 됩니다.
 function* increaseCountHandler(action) {
   try {
     console.log(action.data);
@@ -24,6 +26,8 @@ function* increaseCountHandler(action) {
 }
 
 // 해당 액션의 발생을 감지하는 함수입니다.
+// takeLatest를 통해 제일 마지막 요청만 받고 이전의 요청은 무시하도록 하였습니다.
+// 이 외에도 여러 effects를 제공합니다.
 function* watchIncreaseCounter() {
   yield takeLatest(INCREASE_REQUEST_ACTION, increaseCountHandler);
 }
